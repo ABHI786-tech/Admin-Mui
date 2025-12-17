291d4648ffde1078d317b55e122777eeba06118d
Abhinash Gupta <abhiofficial453@gmail.com>
2025-12-17 22:52:03 +0530

    .

--- src/layout/ProtectedLayout.jsx
+++ src/layout/ProtectedLayout.jsx
@@ -16,7 +16,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import DrawerAppBar from '../components/Navbar';
 import { Outlet } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom'
 import NewNavbar from '../components/NewNavbar';
